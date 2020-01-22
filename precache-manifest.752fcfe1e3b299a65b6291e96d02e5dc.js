dcc3a8aea043ccd1248b51dce2dbc194f896aaaa
Tomasz Zięba <tomasz.zieba89@gmail.com>
2020-01-22 17:50:48 +0100

    Updates

---
+++ precache-manifest.752fcfe1e3b299a65b6291e96d02e5dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277be12be87f4d41580caf4580c1daac",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "255951943582a2e067b9",
+    "url": "/tic-tac-toe/static/css/main.aedd2c91.chunk.css"
+  },
+  {
+    "revision": "9dd3af756797705f509f",
+    "url": "/tic-tac-toe/static/js/2.9683041d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.9683041d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "255951943582a2e067b9",
+    "url": "/tic-tac-toe/static/js/main.66bf6c66.chunk.js"
+  },
+  {
+    "revision": "14c00c4b9a734bdeb7a0",
+    "url": "/tic-tac-toe/static/js/runtime-main.1fec3c3c.js"
+  }
+]);
